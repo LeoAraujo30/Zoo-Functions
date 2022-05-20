3a82c9bf12163e18dc912b460bcb1817a8209969
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-20 18:59:42 -0300

    requisito 3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {}
+  return data.employees.find((pesssoa) => pesssoa.firstName == employeeName || pesssoa.lastName == employeeName)
 }
 
 module.exports = getEmployeeByName;
