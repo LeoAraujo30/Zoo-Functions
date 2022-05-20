ae7c3657d44e1e90b5d4ea3caa429940ef321f6c
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-20 17:41:50 -0300

    requisito 1  feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquiiii
+function getSpeciesByIds(...ids) {
+  if(ids.length === 0) return []
+  if(ids.length >= 1) return data.species.filter((animal) => ids.find((id) => id == animal.id))
 }
 
 module.exports = getSpeciesByIds;
