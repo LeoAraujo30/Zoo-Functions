fae75946f968c0a79bfdd4239a368ea91acd7c22
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-20 18:36:48 -0300

    requisito 2  feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(name, age) {
+  let criatura = data.species.find((animal) => animal.name == name).residents
+  return criatura.every((animal) => animal.age >= age)
 }
 
 module.exports = getAnimalsOlderThan;
