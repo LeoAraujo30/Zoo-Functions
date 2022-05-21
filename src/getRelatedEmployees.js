a403c4f195ddcd4cbc83270380489394bbf4834b
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-20 22:26:31 -0300

    fazendo requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const filtro = data.employees.find((pessoa) => pessoa.id == id).managers
+  if (filtro.length == 0) return true
+  return filtro.some((pessoa) => pessoa == '9e7d4524-363c-416a-8759-8aa7e50c0992')
 }
+// console.log(isManager('c1f50212-35a6-4ecd-8223-f835538526c2'));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  // if (isManager(managerId) == true) 
 }
 
 module.exports = { isManager, getRelatedEmployees };
