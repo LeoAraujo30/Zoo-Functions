ae13d84ca33d781d0734d43924b9afcfb038101e
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-22 19:49:00 -0300

    requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(obj) {
+  if (!obj) {
+    return {
+      [data.species[0].name]: data.species[0].residents.length,
+      [data.species[1].name]: data.species[1].residents.length,
+      [data.species[2].name]: data.species[2].residents.length,
+      [data.species[3].name]: data.species[3].residents.length,
+      [data.species[4].name]: data.species[4].residents.length,
+      [data.species[5].name]: data.species[5].residents.length,
+      [data.species[6].name]: data.species[6].residents.length,
+      [data.species[7].name]: data.species[7].residents.length,
+      [data.species[8].name]: data.species[8].residents.length,
+    };
+  }
+  const { sex } = obj;
+  const filtro = data.species.find((animal) => animal.name === obj.specie).residents;
+  if (!sex) return filtro.length;
+  return filtro.filter((animal) => animal.sex === obj.sex).length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,17 +5,15 @@ function isManager(id) {
   if (filtro.length === 0) return true;
   return filtro.some((pessoa) => pessoa === '9e7d4524-363c-416a-8759-8aa7e50c0992');
 }
-// console.log(isManager('c1f50212-35a6-4ecd-8223-f835538526c2'));
 
 function getRelatedEmployees(managerId) {
-  if (isManager(managerId) == true) {
-    const filtro = data.employees.filter((pessoa) => pessoa.managers.find((id) => id === managerId))
-    return filtro.map((pessoa) => `${pessoa.firstName} ${pessoa.lastName}`)
+  if (isManager(managerId) === true) {
+    const filtro = data.employees.filter((p) => p.managers.find((id) => id === managerId));
+    return filtro.map((pessoa) => `${pessoa.firstName} ${pessoa.lastName}`);
   }
-  if (isManager(managerId) == false) {
-    throw  new  Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 }
-// console.log(getRelatedEmployees('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = { isManager, getRelatedEmployees };
