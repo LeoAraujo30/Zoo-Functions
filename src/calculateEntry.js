c57c4e06ced506a2d89368754f5a07ee1e473d73
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-22 21:15:23 -0300

    requisito 8 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const criança = entrants.filter((pessoa) => pessoa.age < 18).length;
+  const adulto = entrants.filter((pessoa) => pessoa.age >= 18 && pessoa.age < 50).length;
+  const velho = entrants.filter((pessoa) => pessoa.age >= 50).length;
+  return { child: criança, adult: adulto, senior: velho };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (Array.isArray(entrants) === false) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  return (child * data.prices.child) + (adult * data.prices.adult) + (senior * data.prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
