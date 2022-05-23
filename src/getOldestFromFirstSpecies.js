55e69a38b68fe0e925331bf5fe8fab25834bef45
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-05-23 17:51:15 -0300

    requisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idAnimal = data.employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
+  const animalName = data.species.find((animal) => animal.id === idAnimal).residents;
+  const ages = animalName.map((animal) => animal.age);
+  return Object.values(animalName.find((animal) => animal.age === Math.max(...ages)));
 }
 
 module.exports = getOldestFromFirstSpecies;
